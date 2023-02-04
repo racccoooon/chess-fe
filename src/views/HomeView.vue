62d87c6d13c78c0bcbf2b75330e0bda34940e9c4
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-04 16:02:05 +0100

    commit message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold">Chess text</h1>
+    <p>bottom text</p>
+  </div>
 </template>
+
+<script setup lang="ts">
+</script>
