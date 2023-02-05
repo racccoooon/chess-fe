5733ef9fbc663d73a1fa1321a3b18c87cdd92884
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-05 12:22:38 +0100

    it works maybe

--- src/components/BoardRenderer.vue
+++ src/components/BoardRenderer.vue
@@ -1,5 +1,10 @@
 <template>
-  <svg class="w-[800px] aspect-square select-none" :viewBox="`0 0 ${tileAbsoluteWidth * 8 + borderAbsoluteSize * 2} ${tileAbsoluteHeight * 8 + borderAbsoluteSize * 2}`">
+  <svg
+    class="w-[800px] aspect-square select-none"
+    :viewBox="`0 0 ${tileAbsoluteWidth * 8 + borderAbsoluteSize * 2} ${
+      tileAbsoluteHeight * 8 + borderAbsoluteSize * 2
+    }`"
+  >
     <rect width="100%" height="100%" class="fill-gray-100 dark:fill-gray-800" />
     <template v-for="i in 8" :key="i">
       <text
@@ -16,7 +21,9 @@
     <template v-for="i in 8" :key="i">
       <text
         :x="i * tileAbsoluteWidth + borderAbsoluteSize - 18"
-        :y="tileAbsoluteHeight * 8 + borderAbsoluteSize + borderAbsoluteSize / 2"
+        :y="
+          tileAbsoluteHeight * 8 + borderAbsoluteSize + borderAbsoluteSize / 2
+        "
         class="fill-gray-800 dark:fill-gray-100"
         font-size="20"
         font-weight="500"
@@ -25,7 +32,12 @@
         v-text="String.fromCharCode(96 + (reverse ? 9 - i : i))"
       />
     </template>
-    <svg :x="borderAbsoluteSize" :y="borderAbsoluteSize" :width="tileAbsoluteWidth * 8" :height="tileAbsoluteHeight * 8">
+    <svg
+      :x="borderAbsoluteSize"
+      :y="borderAbsoluteSize"
+      :width="tileAbsoluteWidth * 8"
+      :height="tileAbsoluteHeight * 8"
+    >
       <template v-for="x in 8" :key="x">
         <template v-for="y in 8" :key="y">
           <rect
@@ -33,34 +45,52 @@
             :y="(y - 1) * tileAbsoluteHeight"
             :width="tileAbsoluteWidth"
             :height="tileAbsoluteHeight"
-            :class="['fill-green-100','fill-green-500'][reverse ? (x + y + 1) % 2 : (x + y) % 2]"
-            @click="handleClick(reverse ? 8 - x : x - 1, reverse ? y - 1 : 8 - y)"
+            :class="
+              ['fill-green-100', 'fill-green-500'][
+                reverse ? (x + y + 1) % 2 : (x + y) % 2
+              ]
+            "
+            @click="
+              handleClick(reverse ? 8 - x : x - 1, reverse ? y - 1 : 8 - y)
+            "
           />
         </template>
       </template>
       <rect
-          v-if="lastMove !== null"
-          :x="(reverse ? 7 - lastMove.fromCell.x : lastMove.fromCell.x) * tileAbsoluteWidth"
-          :y="(reverse ? lastMove.fromCell.y : 8 - lastMove.fromCell.y - 1) * tileAbsoluteHeight"
-          :width="tileAbsoluteWidth"
-          :height="tileAbsoluteHeight"
-          class="fill-yellow-300/75"
+        v-if="lastMove !== null"
+        :x="
+          (reverse ? 7 - lastMove.fromCell.x : lastMove.fromCell.x) *
+          tileAbsoluteWidth
+        "
+        :y="
+          (reverse ? lastMove.fromCell.y : 8 - lastMove.fromCell.y - 1) *
+          tileAbsoluteHeight
+        "
+        :width="tileAbsoluteWidth"
+        :height="tileAbsoluteHeight"
+        class="fill-yellow-300/75"
       />
       <rect
-          v-if="lastMove !== null"
-          :x="(reverse ? 7 - lastMove.toCell.x : lastMove.toCell.x) * tileAbsoluteWidth"
-          :y="(reverse ? lastMove.toCell.y : 8 - lastMove.toCell.y - 1) * tileAbsoluteHeight"
-          :width="tileAbsoluteWidth"
-          :height="tileAbsoluteHeight"
-          class="fill-yellow-300/75"
+        v-if="lastMove !== null"
+        :x="
+          (reverse ? 7 - lastMove.toCell.x : lastMove.toCell.x) *
+          tileAbsoluteWidth
+        "
+        :y="
+          (reverse ? lastMove.toCell.y : 8 - lastMove.toCell.y - 1) *
+          tileAbsoluteHeight
+        "
+        :width="tileAbsoluteWidth"
+        :height="tileAbsoluteHeight"
+        class="fill-yellow-300/75"
       />
       <rect
-          v-if="currentMove !== null"
-          :x="(reverse ? 7 - currentMove?.fromCell.x : currentMove?.fromCell.x) * tileAbsoluteWidth"
-          :y="(reverse ? currentMove?.fromCell.y : 8 - currentMove?.fromCell.y - 1) * tileAbsoluteHeight"
-          :width="tileAbsoluteWidth"
-          :height="tileAbsoluteHeight"
-          class="fill-red-300/75"
+        v-if="currentMove !== null"
+        :x="selectedCell.x * tileAbsoluteWidth"
+        :y="selectedCell.y * tileAbsoluteHeight"
+        :width="tileAbsoluteWidth"
+        :height="tileAbsoluteHeight"
+        class="fill-red-300/75"
       />
       <TransitionGroup>
         <Piece
@@ -79,9 +109,10 @@
 
 <script setup lang="ts">
 import Piece from "@/components/Piece.vue";
-import type {Board, Move, PartialMove} from "@/lib/types";
+import type { Board, Cell, Move, PartialMove } from "@/lib/types";
+import { computed } from "vue";
 
-defineProps<{
+const props = defineProps<{
   board: Board;
   reverse: boolean;
   currentMove: PartialMove | null;
@@ -100,4 +131,17 @@ const handleClick = (x: number, y: number) => {
   console.log("clicked on board:", x, y);
   emit("click", x, y);
 };
+
+/***
+ * the cell that is currently selected, with reversed board.
+ */
+const selectedCell = computed((): Cell => {
+  let x = props.currentMove?.fromCell?.x || 0;
+  let y = props.currentMove?.fromCell?.y || 0;
+
+  return {
+    x: props.reverse ? 7 - x : x,
+    y: props.reverse ? y : 7 - y,
+  };
+});
 </script>
