5f833e7652982cc779215e0e5a221e9b74179c49
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-11 22:46:48 +0100

    fixed opening search

--- src/lib/chessOpenings.ts
+++ src/lib/chessOpenings.ts
@@ -5,6 +5,11 @@ import type { MoveItem } from "@/lib/types";
 export const getChessOpening = (moves: MoveItem[]) => {
   const notation = getGameNotation(moves, NotationType.Algebraic);
 
+  // sort by longest moves first
+  eco.sort((a, b) => {
+    return b.moves.length - a.moves.length;
+  });
+
   return eco.find((eco) => {
     return notation.startsWith(eco.moves);
   });
