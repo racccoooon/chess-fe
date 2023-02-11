615ead7dc3fc34aa84f8fef4b8308a25b7423c4e
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-11 18:13:59 +0100

    storing settings in local storage

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,15 +1,43 @@
 import { defineStore } from "pinia";
 import { NotationType } from "@/lib/chessNotation";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { get, set, useStorage } from "@vueuse/core";
 
 export const useSettingsStore = defineStore("settings", () => {
-  const notationType = ref(NotationType.Algebraic);
-  const useUnicodeIconsInNotation = ref(true);
-  const raccoonMode = ref(true);
+  // there must be a better way to do this...
 
-  return {
-    notationType,
-    useUnicodeIconsInNotation,
-    raccoonMode,
-  };
+  const settings = useStorage("settings", {
+    notationType: NotationType.Algebraic,
+    useUnicodeIconsInNotation: true,
+    raccoonMode: true,
+  });
+
+  const notationType = ref(get(settings).notationType);
+  const useUnicodeIconsInNotation = ref(
+    get(settings).useUnicodeIconsInNotation
+  );
+  const raccoonMode = ref(get(settings).raccoonMode);
+
+  watch(settings, (newSettings) => {
+    set(notationType, newSettings.notationType);
+    set(useUnicodeIconsInNotation, newSettings.useUnicodeIconsInNotation);
+    set(raccoonMode, newSettings.raccoonMode);
+  });
+
+  watch(notationType, (newNotationType) => {
+    set(settings, { ...get(settings), notationType: newNotationType });
+  });
+
+  watch(useUnicodeIconsInNotation, (newUseUnicodeIconsInNotation) => {
+    set(settings, {
+      ...get(settings),
+      useUnicodeIconsInNotation: newUseUnicodeIconsInNotation,
+    });
+  });
+
+  watch(raccoonMode, (newRaccoonMode) => {
+    set(settings, { ...get(settings), raccoonMode: newRaccoonMode });
+  });
+
+  return { notationType, useUnicodeIconsInNotation, raccoonMode };
 });
