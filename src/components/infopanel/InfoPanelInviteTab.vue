51a8eb129d51cc04b785bcfb60c845b4f33a30ba
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-02-22 17:58:30 +0100

    added qrcode to invite tab

--- src/components/infopanel/InfoPanelInviteTab.vue
+++ src/components/infopanel/InfoPanelInviteTab.vue
@@ -23,14 +23,19 @@
           Share
         </LargeSecondaryButton>
       </div>
+      <div
+        ref="qrCodeContainer"
+        class="w-min rounded-2xl overflow-hidden aspect-square"
+      ></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRoute } from "vue-router";
-import { get, useShare } from "@vueuse/core";
+import { get, useShare, whenever } from "@vueuse/core";
+import QRCodeStyling from "qr-code-styling";
 import LargePrimaryButton from "@/components/forms/LargePrimaryButton.vue";
 import LargeSecondaryButton from "@/components/forms/LargeSecondaryButton.vue";
 
@@ -39,6 +44,8 @@ const playerLinkInput = ref<HTMLInputElement>();
 const gameId = ref(useRoute().params.gameId as string);
 
 const { share, isSupported: shareIsSupported } = useShare();
+
+const qrCodeContainer = ref();
 
 const invitePlayerLink = computed(() => {
   return `${window.location.origin}/play/${get(gameId)}`;
@@ -59,4 +66,43 @@ const copyToClipboard = () => {
 const focusInput = () => {
   get(playerLinkInput)?.select();
 };
+
+const generateQrCode = () => {
+  const qrCode = new QRCodeStyling({
+    width: 200,
+    height: 200,
+    data: get(invitePlayerLink),
+    dotsOptions: {
+      color: "#000000",
+      type: "rounded",
+    },
+    cornersSquareOptions: {
+      type: "square",
+      color: "#000000",
+    },
+    cornersDotOptions: {
+      type: "square",
+      color: "#000000",
+    },
+    backgroundOptions: {
+      color: "#ffffff",
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 0,
+      hideBackgroundDots: true,
+      imageSize: 0.5,
+    },
+  });
+
+  qrCode.append(get(qrCodeContainer));
+};
+
+watch(invitePlayerLink, () => {
+  generateQrCode();
+});
+
+whenever(qrCodeContainer, () => {
+  generateQrCode();
+});
 </script>
