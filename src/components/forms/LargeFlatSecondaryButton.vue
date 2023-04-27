5777904741376957661ae4e3c7df02a949e8a820
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-04-27 19:18:45 +0200

    buttons can now be router links

--- src/components/forms/LargeFlatSecondaryButton.vue
+++ src/components/forms/LargeFlatSecondaryButton.vue
@@ -1,6 +1,6 @@
 <template>
   <component
-    :is="$attrs.href ? 'a' : 'button'"
+    :is="$attrs.href ? 'a' : $attrs.to ? 'RouterLink' : 'button'"
     class="flex items-center justify-center gap-3 px-8 h-16 rounded-xl bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 text-gray-800 dark:text-gray-50 font-medium text-lg transition-all ease-in-out"
   >
     <slot />
